79ef7e1f47c90ab852e4d90b55960fa995284554
Rama Bena <ramabena.rb@gmail.com>
2022-04-06 01:52:17 +0800

    [wordsapp] buat class fragment

--- android-basics-kotlin-words-app-starter/app/src/main/java/com/example/wordsapp/WordListFragment.kt
+++ android-basics-kotlin-words-app-starter/app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -1,8 +1,49 @@
 package com.example.wordsapp
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wordsapp.databinding.FragmentWordListBinding
 
 
 class WordListFragment : Fragment() {
+    private var _binding: FragmentWordListBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
 
+    companion object {
+        const val LETTER = "letter"
+        const val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentWordListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val letterId = activity?.intent?.extras?.getString(LETTER).toString()
+        recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = WordAdapter(letterId, requireContext())
+
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+        )
+        activity?.title = getString(R.string.detail_prefix) + " " + letterId
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
