a1d2b402d0817db88bf6cd164ef5534ee43ba981
rama_pc_nara <ramabena.rb@gmail.com>
2022-04-08 15:54:12 +0800

    [unscramble] buat GameViewModel dan edit GameFragment

---
+++ android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -0,0 +1,89 @@
+package com.example.android.unscramble.ui.game
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
+    private var _score = 0
+    val score: Int
+        get() = _score
+
+    private var _currentWordCount = 1
+    val currentWordCount: Int
+        get() = _currentWordCount
+
+    private lateinit var _currentScrambledWord: String
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
+
+/*    private fun oldGetNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++currentWordCount
+            wordsList.add(currentWord)
+        }
+    }*/
+
+    private fun getNextWord() {
+        var tempWord = allWordsList.random()
+
+        while (wordsList.contains(tempWord)) {
+            tempWord = allWordsList.random()
+        }
+
+        currentWord = tempWord
+        val tempScramble = currentWord.toCharArray()
+
+        while (String(tempScramble).equals(currentWord, false)) {
+            tempScramble.shuffle()
+        }
+
+        _currentScrambledWord = String(tempScramble)
+        _currentWordCount++
+        wordsList.add(currentWord)
+    }
+
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+
+    /*
+    * Returns true if the current word count is less than MAX_NO_OF_WORDS.
+    * Updates the next word.
+    */
+    fun nextWord(): Boolean {
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
+
+    fun isUserWordCorrect(playerWord: String): Boolean {
+        return if (playerWord.equals(currentWord, true)) {
+            increaseScore()
+            true
+        } else false
+    }
+
+    private fun increaseScore() {
+        _score += SCORE_INCREASE
+    }
+}
