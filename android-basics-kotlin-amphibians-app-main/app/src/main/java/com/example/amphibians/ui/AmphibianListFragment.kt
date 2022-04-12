4136739d752200a67fbe57564233ab18486ebb1c
Rama Bena <ramabena.rb@gmail.com>
2022-04-13 02:28:51 +0800

    [amphibians] selesai testing

--- android-basics-kotlin-amphibians-app-main/app/src/main/java/com/example/amphibians/ui/AmphibianListFragment.kt
+++ android-basics-kotlin-amphibians-app-main/app/src/main/java/com/example/amphibians/ui/AmphibianListFragment.kt
@@ -35,7 +35,7 @@ class AmphibianListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentAmphibianListBinding.inflate(inflater)
-        // TODO: call the view model method that calls the amphibians api
+//        viewModel.getAmphibian()
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
         binding.recyclerView.adapter = AmphibianListAdapter(AmphibianListener { amphibian ->
--- android-basics-kotlin-amphibians-app-main/app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
+++ android-basics-kotlin-amphibians-app-main/app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
@@ -15,24 +15,49 @@
  */
 package com.example.amphibians.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.amphibians.network.Amphibian
+import com.example.amphibians.network.AmphibianApi
+import kotlinx.coroutines.launch
 
 enum class AmphibianApiStatus {LOADING, ERROR, DONE}
 
 class AmphibianViewModel : ViewModel() {
+    /**Property*/
+    private val _status = MutableLiveData<AmphibianApiStatus>()
+    val status: LiveData<AmphibianApiStatus> = _status
 
-    // TODO: Create properties to represent MutableLiveData and LiveData for the API status
+    private val _amphibianList = MutableLiveData<List<Amphibian>>()
+    val amphibianList: LiveData<List<Amphibian>> = _amphibianList
 
-    // TODO: Create properties to represent MutableLiveData and LiveData for a list of amphibian objects
+    private val _amphibian = MutableLiveData<Amphibian>()
+    val amphibian: LiveData<Amphibian> = _amphibian
 
-    // TODO: Create properties to represent MutableLiveData and LiveData for a single amphibian object.
-    //  This will be used to display the details of an amphibian when a list item is clicked
+    /**Init*/
+    init {
+        getAmphibian()
+    }
 
-    // TODO: Create a function that gets a list of amphibians from the api service and sets the
-    //  status via a Coroutine
-
+    /**Method*/
     fun onAmphibianClicked(amphibian: Amphibian) {
-        // TODO: Set the amphibian object
+        _amphibian.value = amphibian
+    }
+
+    /**Private method / helper */
+    private fun getAmphibian() {
+        viewModelScope.launch {
+            _status.value = AmphibianApiStatus.LOADING
+            try {
+                _amphibianList.value = AmphibianApi.retrofitService.getAmphibian()
+                _status.value = AmphibianApiStatus.DONE
+            }
+            catch (e: Exception) {
+                _amphibianList.value = listOf()
+                _status.value = AmphibianApiStatus.ERROR
+            }
+        }
     }
 }
