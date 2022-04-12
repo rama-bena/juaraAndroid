4b600710b961a67786e5fd4c09110220bf5430fc
rama_pc_nara <ramabena.rb@gmail.com>
2022-04-12 17:09:44 +0800

    [mars photos] selesai testing

---
+++ android-basics-kotlin-mars-photos-app-starter/app/src/test/java/com/example/android/marsphotos/MarsApiServiceTest.kt
@@ -0,0 +1,48 @@
+package com.example.android.marsphotos
+
+import com.example.android.marsphotos.network.MarsApiService
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import junit.framework.TestCase.assertNotNull
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+@RunWith(JUnit4::class)
+class MarsApiServiceTest: BaseTest() {
+    private lateinit var service: MarsApiService
+
+    @Before
+    fun setup() {
+        val url = mockWebServer.url("/")
+        service = Retrofit.Builder()
+            .baseUrl(url)
+            .addConverterFactory(
+                MoshiConverterFactory.create(
+                Moshi.Builder()
+                    .add(KotlinJsonAdapterFactory())
+                    .build()
+            ))
+            .build()
+            .create(MarsApiService::class.java)
+    }
+
+    @Test
+    fun api_service() {
+        enqueue("mars_photos.json")
+        runBlocking {
+            val apiResponse = service.getPhotos()
+
+            assertNotNull(apiResponse)
+            assertTrue("The list was empty", apiResponse.isNotEmpty())
+            assertEquals("The IDs did not match", "424905", apiResponse[0].id)
+        }
+    }
+
+}
