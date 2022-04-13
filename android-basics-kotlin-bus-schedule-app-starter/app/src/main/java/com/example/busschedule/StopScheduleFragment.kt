fbd3d9584ff005ff8334d042ff65570b5b08d6a6
rama_pc_nara <ramabena.rb@gmail.com>
2022-04-13 16:51:21 +0800

    [bus schedule] sampai bisa install

--- android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/StopScheduleFragment.kt
+++ android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/StopScheduleFragment.kt
@@ -20,23 +20,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.busschedule.databinding.StopScheduleFragmentBinding
+import com.example.busschedule.viewmodels.BusScheduleViewModel
+import com.example.busschedule.viewmodels.BusScheduleViewModelFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class StopScheduleFragment: Fragment() {
 
+    /**PROPERTY*/
     companion object {
         var STOP_NAME = "stopName"
     }
-
     private var _binding: StopScheduleFragmentBinding? = null
-
     private val binding get() = _binding!!
-
     private lateinit var recyclerView: RecyclerView
-
     private lateinit var stopName: String
+    private val viewModel: BusScheduleViewModel by activityViewModels {
+        BusScheduleViewModelFactory(
+            (activity?.application as BusScheduleApplication).database.scheduleDao()
+        )
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,6 +69,17 @@ class StopScheduleFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
         recyclerView = binding.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        val busStopAdapter = BusStopAdapter({})
+        recyclerView.adapter = busStopAdapter
+
+        // submitList() is a call that accesses the database. To prevent the
+        // call from potentially locking the UI, you should use a
+        // coroutine scope to launch the function. Using GlobalScope is not
+        // best practice, and in the next step we'll see how to improve this.
+        GlobalScope.launch(Dispatchers.IO) {
+            busStopAdapter.submitList(viewModel.scheduleForStopName(stopName))
+        }
     }
 
     override fun onDestroyView() {
